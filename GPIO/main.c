39db69e0b2645759bbfb38799295c63c00d24bf8
esteban <esteban@kalii>
2016-08-25 16:19:59 -0600

    Test version

--- GPIO/main.c
+++ GPIO/main.c
@@ -5,10 +5,14 @@
 #include <unistd.h>
 
 void pinMode(int pin, char* MODE) {
-    
+
     //Inicialización del pin
+    if (pin < 1 || pin > 40) {
+        return;
+    }
+
     FILE *fp;
-    fp = fopen("/sys/class/gpio/export", "a+");
+    fp = fopen("/sys/class/gpio/export", "a");
     fprintf(fp, "%d", pin);
     fclose(fp);
 
@@ -17,13 +21,12 @@ void pinMode(int pin, char* MODE) {
     char str[50] = "/sys/class/gpio/gpio";
     char aInt[2];
 
-    snprintf(aInt, 15, "%d", pin);
-
+    snprintf(aInt, 3, "%d", pin);
     strcat(str, aInt);
     strcat(str, "/direction");
     printf("%s\n", str);
 
-    fp2 = fopen(str, "a");
+    fp2 = fopen(str, "w");
 
     if (MODE == "OUTPUT") {
         char x[3] = "out";
@@ -31,29 +34,29 @@ void pinMode(int pin, char* MODE) {
     } else if (MODE == "INPUT") {
         char x[2] = "in";
         fwrite(x, sizeof (x[0]), sizeof (x) / sizeof (x[0]), fp2);
-    } else {
-        //MODO DESCONOCIDO
     }
 
     fclose(fp2);
-
 }
 
 void digitalWrite(int pin, int value) {
     FILE *fp;
-    char str[35] = "/sys/class/gpio/gpio";
-    char aInt[15];
-    snprintf(aInt, 15, "%d", pin);
 
-    //strcat(str, "/sys/class/gpio/gpio");
+    if (pin < 1 || pin > 40) {
+        return;
+    }
+
+    char str[35] = "/sys/class/gpio/gpio";
+    char aInt[2];
+    snprintf(aInt, 3, "%d", pin);
     strcat(str, aInt);
     strcat(str, "/value");
 
-    fp = fopen(str, "a");
+    printf("%s\n", str);
+
+    fp = fopen(str, "w");
     if (value == 0 || value == 1) {
-        fwrite(&value, sizeof (value), sizeof (value) / sizeof (value), fp);
-    } else {
-        //MODO DESCONOCIDO
+        fprintf(fp, "%d", value);
     }
     fclose(fp);
 
@@ -62,19 +65,31 @@ void digitalWrite(int pin, int value) {
 int digitalRead(int pin) {
     FILE *fp;
     char str[35] = "/sys/class/gpio/gpio";
-    char aInt[15];
-    char value[5];
-    snprintf(aInt, 15, "%d", pin);
+    char aInt[2];
+    char value[2];
 
-    //strcat(str, "/sys/class/gpio/gpio");
+    snprintf(aInt, 3, "%d", pin);
     strcat(str, aInt);
     strcat(str, "/value");
 
+    printf("%s\n", str);
     fp = fopen(str, "r");
-    fread(value, 5, 1, fp);
-    printf("%s\n", value);
+    if (fp != NULL) {
+        fread(value, 2, 1, fp);
+        printf("%s\n", value);
+        fclose(fp);
+        return atoi(value);
+    } else {
+        return -1;
+    }
+}
+
+void pinUnload(int pin) {
+
+    FILE *fp;
+    fp = fopen("/sys/class/gpio/unexport", "a");
+    fprintf(fp, "%d", pin);
     fclose(fp);
-    return atoi(value);
 }
 
 void blink(int pin, int freq, int duration) {
@@ -93,9 +108,15 @@ void blink(int pin, int freq, int duration) {
 
 int main() {
     //blink(5, 4, 10);
-    pinMode(5, "INPUT");
- //   digitalWrite(5,1);
-    sleep(2);
- //   digitalWrite(5,0);
+    //  pinMode(5, "INPUT");
+    // digitalWrite(5,1);
+    // sleep(1);
+    // digitalWrite(7,0);
+    // sleep(1);
+    // int x5 = digitalRead(4);
+    //digitalWrite(40,1);
+    //sleep(0.1);
+    //digitalRead(40);
+
     return 0;
 }
