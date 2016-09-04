1a40e07cb645789014692aad3dae7443c15a33ba
esteban <esteban@kalii>
2016-09-04 11:50:16 -0600

    Camara envía imagen local codificada a base64

--- routes/camara.js
+++ routes/camara.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var fs = require('fs');
+//var gpio = require("../build/Release/gpio")
 var router = express.Router();
 
 // Middleware for all this routers requests
@@ -11,10 +13,21 @@ router.use(function timeLog(req, res, next) {
 router.route('/')
         .get(function (req, res) {
             console.log("FOTO");
-            res.send(true);
+            //var path= gpio??
+            //var base64str = base64_encode(path);
+            var base64str = base64_encode('./images/a.png');
+            res.send(base64str);
         });
 
 module.exports = router;
+
+// function to encode file data to base64 encoded string
+function base64_encode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+}
 
 function dateDisplayed(timestamp) {
     var date = new Date(timestamp);
